f4eabed8591834386905758f2c4723e3cf07cf40
Sviatoslav Fedorov <trental.76@gmail.com>
2024-09-16 11:57:38 +0300

    Add project files.

---
+++ VNPCheckerService/Program.cs
@@ -0,0 +1,12 @@
+﻿using VNPCheckerService;
+
+if (VPNCheckerService.IsVpnConnected())
+{
+    Console.WriteLine("VPN connected");
+    Environment.Exit(0);
+}
+else
+{
+    Console.WriteLine("No VPN!!!");
+    Environment.Exit(1);
+}
---
+++ VNPCheckerService/VPNCheckerService.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.NetworkInformation;
+
+namespace VNPCheckerService;
+
+internal class VPNCheckerService
+{
+    private const string VPNConnectionName = "Cisco AnyConnect";
+
+    public static bool IsVpnConnected()
+    {
+        NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+        return interfaces.Any(x =>
+            x.OperationalStatus == OperationalStatus.Up
+            && x.Description.StartsWith(VPNConnectionName));
+    }
+}
